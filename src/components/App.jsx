d402510b99974eab741772928cc99a3354161689
Riccardo Civiero <riccardociviero98@icloud.com>
2024-11-02 00:42:03 +0100

    analytics installed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 // Components
 import { Header } from "./Header.jsx";
 import { Invoice } from "./Invoice.jsx";
+import { Analytics } from "@vercel/analytics/react"
 
 // Variables
 import { coefficientiReddito, imposteSostitutive } from "../back-end/formule.js";
@@ -11,6 +12,7 @@ export const App = () => {
 
   return (
     <>
+      <Analytics />
       <Header />
       <Invoice
         coefficientiReddito={coefficientiReddito}
