9697cc0ba7472b1a42388f0f245f2d01918206d7
Riccardo Civiero <riccardociviero98@icloud.com>
2024-12-20 10:32:55 +0100

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <>
             <footer className="mt-12 sticky top-0 bg-gradient-to-r from-blue-500 to-purple-600 shadow-lg z-50">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                    <span className="text-sm text-white sm:text-center">© 2024 Tutti i diritti riservati.
+                    <span className="text-sm text-white sm:text-center">© 2024 Tutti i diritti riservati. Realizzato da <a href="https://ricciviero.dev" target="_blank" rel="noreferrer" className="text-blue-200 hover:text-blue-300 transition-colors duration-300 ease-in-out">Riccardo Civiero</a>
                     </span>
                     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
 
